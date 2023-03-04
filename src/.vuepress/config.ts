bff8b1bc813f144f36604aa12729e8e409a322b3
Andreas Zeller <andreas@zeller.sh>
2023-03-04 22:04:59 +0100

    removed some localisation

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,14 +6,9 @@ import theme from "./theme.js";
 // config https://vuepress-theme-hope.github.io/v2/cookbook/tutorial/config.html#config-scope
 export default defineUserConfig({
   base: "/",
-
-  locales: {
-    "/": {
-      lang: "en-US",
-      title: "Zeller.sh",
-      description: "Here I write about Projects or Topics I stumble about while building my Homelab.",
-    },
-  },
+  title: "Zeller.sh",
+  description: "Here I write about Projects or Topics I stumble about while building my Homelab.",
+  
   plugins: [
     searchPlugin({
       // your options
